5cc76100144506fc7949876fe8ae6033e026a7bf
paulyd123 <g00297086@gmit.ie>
2016-11-11 15:26:31 +0000

    Main page finished after fixing error

---
+++ UWPCrestGuess/UWPCrestGuess/Model/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPCrestGuess.Model
+{
+    class Question
+    {
+        public int ID { get; set; }
+        public String Image { get; set; }
+        public String AnswerA { get; set; }
+        public String AnswerB { get; set; }
+        public String AnswerC { get; set; }
+        public String AnswerD { get; set; }
+        public String CorrectAnswer { get; set; }
+    }
+}
