55da439da0a0d48183518c5ac8b31fd28439730a
paulyd123 <g00297086@gmit.ie>
2016-12-05 19:01:35 +0000

    Creating question

--- UWPCrestGuess/UWPCrestGuess/Play.xaml.cs
+++ UWPCrestGuess/UWPCrestGuess/Play.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UWPCrestGuess.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,10 +24,37 @@ namespace UWPCrestGuess
     /// </summary>
     public sealed partial class Play : Page
     {
+        List<Question> listQuestion = new List<Question>();
+        DBHelper db;
+        int index = 0;
+        int score = 0;
+        int correctAnswer = 0;
+        int totalQuestion = 0;
+        int thisQuestion = 1;
+
+
         public Play()
         {
             this.InitializeComponent();
+            db = new DBHelper();
         }
+
+        public void showQuestion(int index)
+        {
+            if(index < listQuestion.Count)
+            {
+                string imgUrl = "ms-appx:///Assets/crests/" + listQuestion[index].Image + ".jpg";
+                imageLoad.Source = new BitmapImage(new Uri(imgUrl));
+
+                answerA.Content = listQuestion[index].AnswerA;
+                answerB.Content = listQuestion[index].AnswerB;
+                answerC.Content = listQuestion[index].AnswerC;
+                answerD.Content = listQuestion[index].AnswerD;
+            }
+
+        
+        }
+      
 
     }
 }
